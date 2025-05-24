e93d6dfa72a13e98e718c4470736e97a0ed54d86
morehao <morehao@qq.com>
2025-05-24 19:31:10 +0800

    feat(example): rename _example

--- cmd/root.go
+++ cmd/root.go
@@ -12,7 +12,7 @@ var rootCmd = &cobra.Command{
 	Use:   "gocli",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+examples and usage of using your application. For _example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
